8e0d5582a72f8658f8e23abd3f6bcfa660f83ec7
Dalep <dalepaulsen.code.social@gmail.com>
2019-11-11 03:35:57 +1000

    added stripe with custom checkout form and custom CC validation

--- src/app/assets/javascripts/application.js
+++ src/app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
+//
 //= require jquery
-//= require bootstrap-sprockets
+//= require jquery.turbolinks
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require activestorage
+//= require rails-ujs
 //= require_tree .
